2c4f936f2f614e3b3eb6dae0a99a9d106e3f39f4
Daniel Frank <git@danielfrank.net>
2020-03-15 13:01:16 +0100

    writefreely: mark as broken and import assets from their precompiled binaries

--- pkgs/writefreely/default.nix
+++ pkgs/writefreely/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, lib, fetchFromGitHub, buildGoModule, go-bindata }:
+{ stdenv, lib, fetchFromGitHub, fetchurl, buildGoModule, go-bindata,
+}:
 
 buildGoModule rec {
   name = "writefreely-${version}";
@@ -11,6 +12,11 @@ buildGoModule rec {
     sha256 = "1h6mcsb74ybp4dksylh2avyvgcrc232ny9l1075pmslhqr85kbvv";
   };
 
+  assets = fetchurl {
+    url = "https://github.com/writeas/writefreely/releases/download/v${version}/writefreely_${version}_linux_amd64.tar.gz";
+    sha256 = "0kahp93jj6irgcl692ddd7p87zh7gxwrs3arw5lbrf77jr1jgvkm";
+  };
+
   buildInputs = [ go-bindata ];
 
   modSha256 = "140rh8hfz133flng72j4p9zsmvv05i0330ngsz4b90p2kaavfqqz";
@@ -18,12 +24,20 @@ buildGoModule rec {
     go-bindata -pkg writefreely -ignore=\\.gitignore -tags="!wflib" schema.sql sqlite.sql
     '';
   buildFlagsArray = [ "-tags='sqlite'" ];
+  postInstall = ''
+    mkdir -p $out/lib
+    cd $out/lib
+    tar xavf ${assets} writefreely/{pages,static,templates}
+    mv writefreely/* .
+    rmdir writefreely
+    '';
 
   meta = with lib; {
+    broken = true;
     description = "A simple, federated blogging platform";
     homepage = https://writefreely.org;
     license = licenses.agpl3;
-    maintainers = with maintainers; [ matthiasbeyer ];
+    maintainers = with maintainers; [ tokudan ];
     platforms = platforms.linux;
   };
 }
